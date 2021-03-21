2f926cdfbe4eb4092826f53b80c4d05e9831d8c4
Oleksandr Afanasiev <afanasievov@gmail.com>
2021-03-21 13:42:12 +0200

    validate task status

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsIn, IsOptional, IsNotEmpty } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn(Object.values(TaskStatus))
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
