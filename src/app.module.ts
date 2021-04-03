c5c7c125b56915d5ed2107091951fd98442d6817
Oleksandr Afanasiev <o.afanasiev@mobidev.biz>
2021-04-03 14:10:29 +0300

    - add graphql packages -add lesson module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { PassportModule } from '@nestjs/passport';
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { sequelizeConfig } from './config/sequelize.app.config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [SequelizeModule.forRoot(sequelizeConfig), TasksModule, AuthModule, PassportModule],
+  imports: [
+    SequelizeModule.forRoot(sequelizeConfig),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    TasksModule,
+    AuthModule,
+    PassportModule,
+    LessonModule,
+  ],
 })
 export class AppModule {}
